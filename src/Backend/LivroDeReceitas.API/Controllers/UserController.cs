70cf956abe5aa974836eb27833d922b96ba4f698
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2025-07-08 09:47:46 -0300

    feat: Adicionado endpoint para registrar um usuário

---
+++ src/Backend/LivroDeReceitas.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using LivroDeReceitas.Comunication.Requests;
+using LivroDeReceitas.Comunication.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivroDeReceitas.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson requestRegisterUserJson)
+        {
+            return Created();
+        }
+    }
+}
