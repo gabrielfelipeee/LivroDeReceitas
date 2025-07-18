e5bb08cac46a39a095e4054a0622aeb0cd609395
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2025-07-18 17:27:37 -0300

    test: Adiciona teste de validação para a requisição de registro de usuário

---
+++ tests/CommomTestUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using LivroDeReceitas.Comunication.Requests;
+using Bogus;
+
+namespace CommomTestUtilities.Requests
+{
+    public class RequestRegisterUserJsonBuilder
+    {
+        public static RequestRegisterUserJson Build(int passwordLength = 10)
+        {
+            return new Faker<RequestRegisterUserJson>()
+                .RuleFor(user => user.Name, (faker) => faker.Person.FirstName)
+                .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
+                .RuleFor(user => user.Password, (faker) => faker.Internet.Password(passwordLength));
+        }
+    }
+}
---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,89 @@
+﻿using CommomTestUtilities.Requests;
+using LivroDeReceitas.Application.UseCases.User.Register;
+using LivroDeReceitas.Exceptions;
+using Shouldly;
+
+namespace Validators.Test.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            var result = validator.Validate(request);
+
+            Assert.True(result.IsValid); // Forma nativa
+            result.IsValid.ShouldBeTrue(); // Espera que o resultado seja sucesso
+        }
+
+        [Fact]
+        public void Error_NameEmpty()
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1); // Verifica se contém exatamente um único item
+
+            // Espera que a mensagem de erro do primeiro item da lista de erros seja igual à ResourceMessagesException.NAME_EMPTY
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceMessagesException.NAME_EMPTY);
+        }
+
+        [Fact]
+        public void Error_EmailEmpty()
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = string.Empty;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceMessagesException.EMAIL_EMPTY);
+        }
+
+        [Fact]
+        public void Error_EmailInvalid()
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = "emailinvalid";
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceMessagesException.EMAIL_INVALID);
+        }
+
+        // O teste será executado 6 vezes, com os valores 0, 1, 2, 3, 4 e 5 sendo passados como argumento para o parâmetro passwordLength
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Error_PasswordInvalid(int passwordLength)
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build(passwordLength);
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceMessagesException.PASSWORD_MUST_BE_LONGER_THAN_6_CHARACTERS);
+        }
+    }
+}
