418398cbe1d778cdc1dc46018fad367b05958c21
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2025-07-22 19:41:05 -0300

    feat: Adiciona Request para fazer login

---
+++ src/Shared/LivroDeReceitas.Comunication/Requests/RequestLoginJson.cs
@@ -0,0 +1,8 @@
+﻿namespace LivroDeReceitas.Comunication.Requests
+{
+    public class RequestLoginJson
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
