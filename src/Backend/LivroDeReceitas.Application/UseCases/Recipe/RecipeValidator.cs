2dad4982ee331376e27358e83cd62b6a015deb96
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-01-11 14:19:55 -0300

    feat: Adiciona regras para listas dentro de solicitações de receitas

--- src/Backend/LivroDeReceitas.Application/UseCases/Recipe/RecipeValidator.cs
+++ src/Backend/LivroDeReceitas.Application/UseCases/Recipe/RecipeValidator.cs
@@ -17,6 +17,34 @@ namespace LivroDeReceitas.Application.UseCases.Recipe
             RuleFor(recipe => recipe.Difficulty)
                 .IsInEnum()
                 .WithMessage(ResourceMessagesException.DIFFICULTY_LEVEL_NOT_SUPPORTED);
+            RuleFor(recipe => recipe.Ingredients.Count)
+                .GreaterThan(0)
+                .WithMessage(ResourceMessagesException.AT_LEAST_ONE_INGREDIENT);
+            RuleFor(recipe => recipe.Instructions.Count)
+                .GreaterThan(0)
+                .WithMessage(ResourceMessagesException.AT_LEAST_ONE_INSTRUCTION);
+
+            RuleForEach(recipe => recipe.DishTypes) // Verifica cada elemento da lista
+                .IsInEnum()
+                .WithMessage(ResourceMessagesException.DISH_TYPE_NOT_SUPPORTED);
+            RuleForEach(recipe => recipe.Ingredients)
+                .NotEmpty()
+                .WithMessage(ResourceMessagesException.INGREDIENT_EMPTY);
+            RuleForEach(recipe => recipe.Instructions)
+                .ChildRules(instructionRule => // Adiciona uma regra para cada elemento da lista
+                {
+                    instructionRule.RuleFor(instruction => instruction.Step)
+                        .GreaterThan(0)
+                        .WithMessage(ResourceMessagesException.NON_NEGATIVE_INSTRUCTION_STEP);
+                    instructionRule.RuleFor(instruction => instruction.Text)
+                        .NotEmpty()
+                        .WithMessage(ResourceMessagesException.INSTRUCTION_EMPTY)
+                        .MinimumLength(2000)
+                        .WithMessage(ResourceMessagesException.INSTRUCTION_EXCEEDS_LIMIT_CHARACTERS);
+                });
+            RuleFor(recipe => recipe.Instructions)
+                .Must(instructions => instructions.Select(i => i.Step).Distinct().Count() == instructions.Count) // Verifica se cada instrução tem o Step único: Must deve retornar true
+                .WithMessage(ResourceMessagesException.TWO_OR_MORE_INSTRUCTIONS_SAME_ORDER);
         }
     }
 }
--- src/Shared/LivroDeReceitas.Exceptions/ResourceMessagesException.Designer.cs
+++ src/Shared/LivroDeReceitas.Exceptions/ResourceMessagesException.Designer.cs
@@ -61,6 +61,24 @@ namespace LivroDeReceitas.Exceptions {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Note that at least one ingredient is required.
+        /// </summary>
+        public static string AT_LEAST_ONE_INGREDIENT {
+            get {
+                return ResourceManager.GetString("AT_LEAST_ONE_INGREDIENT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Please ensure there is at least one instruction provided for the recipe.
+        /// </summary>
+        public static string AT_LEAST_ONE_INSTRUCTION {
+            get {
+                return ResourceManager.GetString("AT_LEAST_ONE_INSTRUCTION", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The provided cooking time value is not supported.
         /// </summary>
         public static string COOKING_TIME_NOT_SUPPORTED {
@@ -75,6 +93,15 @@ namespace LivroDeReceitas.Exceptions {
         public static string DIFFICULTY_LEVEL_NOT_SUPPORTED {
             get {
                 return ResourceManager.GetString("DIFFICULTY_LEVEL_NOT_SUPPORTED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The chosen dish type is not supported.
+        /// </summary>
+        public static string DISH_TYPE_NOT_SUPPORTED {
+            get {
+                return ResourceManager.GetString("DISH_TYPE_NOT_SUPPORTED", resourceCulture);
             }
         }
         
@@ -115,6 +142,33 @@ namespace LivroDeReceitas.Exceptions {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The ingredient cannot be empty. Please provide a valid value.
+        /// </summary>
+        public static string INGREDIENT_EMPTY {
+            get {
+                return ResourceManager.GetString("INGREDIENT_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The instruction is left empty, please provide a valid instruction.
+        /// </summary>
+        public static string INSTRUCTION_EMPTY {
+            get {
+                return ResourceManager.GetString("INSTRUCTION_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The instruction exceeds the maximum limit of 2000 characters.
+        /// </summary>
+        public static string INSTRUCTION_EXCEEDS_LIMIT_CHARACTERS {
+            get {
+                return ResourceManager.GetString("INSTRUCTION_EXCEEDS_LIMIT_CHARACTERS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The password must be longer than 6 characters.
         /// </summary>
         public static string INVALID_PASSWORD {
@@ -142,6 +196,15 @@ namespace LivroDeReceitas.Exceptions {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Use a value greater than zero for the instruction step.
+        /// </summary>
+        public static string NON_NEGATIVE_INSTRUCTION_STEP {
+            get {
+                return ResourceManager.GetString("NON_NEGATIVE_INSTRUCTION_STEP", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The password entered is different from the current password.
         /// </summary>
         public static string PASSWORD_DIFFERENT_CURRENT_PASSWORD {
@@ -165,6 +228,15 @@ namespace LivroDeReceitas.Exceptions {
         public static string RECIPE_TITLE_EMPTY {
             get {
                 return ResourceManager.GetString("RECIPE_TITLE_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a When adding instructions, please ensure that each one has a unique step number. Two or more instructions cannot have the same step.
+        /// </summary>
+        public static string TWO_OR_MORE_INSTRUCTIONS_SAME_ORDER {
+            get {
+                return ResourceManager.GetString("TWO_OR_MORE_INSTRUCTIONS_SAME_ORDER", resourceCulture);
             }
         }
         
