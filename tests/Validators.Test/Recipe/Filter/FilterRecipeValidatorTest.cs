8a246b56ba2dd211f79c5e578a222c4b2382ff59
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-01-18 23:52:00 -0300

    test: Adiciona testes para filtro de receitas

---
+++ tests/Validators.Test/Recipe/Filter/FilterRecipeValidatorTest.cs
@@ -0,0 +1,68 @@
+﻿using CommomTestUtilities.Requests;
+using LivroDeReceitas.Application.UseCases.Recipe.Filter;
+using LivroDeReceitas.Comunication.Enums;
+using LivroDeReceitas.Exceptions;
+using Shouldly;
+
+namespace Validators.Test.Recipe.Filter
+{
+    public class FilterRecipeValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new FilterRecipeValidator();
+
+            var request = RequestFilterRecipeJsonBuilder.Build();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Error_InvalidCookingTime()
+        {
+            var validator = new FilterRecipeValidator();
+
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            request.CookingTimes.Add((CookingTime)1000);
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceMessagesException.COOKING_TIME_NOT_SUPPORTED);
+        }
+
+        [Fact]
+        public void Error_InvalidDifficulty()
+        {
+            var validator = new FilterRecipeValidator();
+
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            request.Difficulties.Add((Difficulty)1000);
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceMessagesException.DIFFICULTY_LEVEL_NOT_SUPPORTED);
+        }
+
+        [Fact]
+        public void Error_InvalidDishTypes()
+        {
+            var validator = new FilterRecipeValidator();
+
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            request.DishTypes.Add((DishType)1000);
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceMessagesException.DISH_TYPE_NOT_SUPPORTED);
+        }
+    }
+}
---
+++ tests/WebApi.Test/Recipe/Filter/FilterRecipeTest.cs
@@ -0,0 +1,102 @@
+﻿using CommomTestUtilities.Requests;
+using CommomTestUtilities.Tokens;
+using LivroDeReceitas.Comunication.Requests;
+using LivroDeReceitas.Exceptions;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Recipe.Filter
+{
+    public class FilterRecipeTest : LivroDeReceitasClassFixture
+    {
+        private const string METHOD = "recipe/filter";
+        private readonly Guid _userIdentifier;
+
+        private string _recipeTitle;
+        private LivroDeReceitas.Domain.Enums.Difficulty _recipeDifficultyLevel;
+        private LivroDeReceitas.Domain.Enums.CookingTime _recipeCookingTime;
+        private IList<LivroDeReceitas.Domain.Enums.DishType> _recipeDishTypes;
+
+        public FilterRecipeTest(CustomWebApplicationFactory factory) : base(factory)
+        {
+            _userIdentifier = factory.GetUserIdentifier();
+
+            _recipeTitle = factory.GetRecipeTitle();
+            _recipeDifficultyLevel = factory.GetRecipeDifficulty();
+            _recipeCookingTime = factory.GetRecipeCookingTime();
+            _recipeDishTypes = factory.GetRecipeDishTypes();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = new RequestFilterRecipeJson
+            {
+                RecipeTitle_Ingredient = _recipeTitle,
+                CookingTimes = [(LivroDeReceitas.Comunication.Enums.CookingTime)_recipeCookingTime],
+                Difficulties = [(LivroDeReceitas.Comunication.Enums.Difficulty)_recipeDifficultyLevel],
+                DishTypes = _recipeDishTypes.Select(dishType => (LivroDeReceitas.Comunication.Enums.DishType)dishType).ToList()
+            };
+
+            var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+            var response = await DoPost(method: METHOD, request: request, token: token);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            responseData.RootElement.GetProperty("recipes").EnumerateArray().ToList()
+                .ShouldSatisfyAllConditions(recipes =>
+                {
+                    recipes.ShouldNotBeNull();
+                });
+        }
+
+        [Fact]
+        public async Task Success_NoContent()
+        {
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            request.RecipeTitle_Ingredient = "recipeDontExist";
+
+            var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+            var response = await DoPost(method: METHOD, request: request, token: token);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+        }
+
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_CookingTimeInvalid(string culture)
+        {
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            request.CookingTimes.Add((LivroDeReceitas.Comunication.Enums.CookingTime)1000);
+
+            var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+            var response = await DoPost(method: METHOD, request: request, token: token, culture: culture);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var expectedMessage = ResourceMessagesException.ResourceManager.GetString("COOKING_TIME_NOT_SUPPORTED", new CultureInfo(culture));
+
+            responseData.RootElement.GetProperty("errors").EnumerateArray().ToList()
+                .ShouldSatisfyAllConditions(errors =>
+                {
+                    errors.Count.ShouldBe(1);
+                    errors.Single().GetString().ShouldBe(expectedMessage);
+                });
+        }
+    }
+}
