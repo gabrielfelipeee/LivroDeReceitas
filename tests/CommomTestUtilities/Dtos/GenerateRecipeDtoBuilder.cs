2509d9bed4d887b888117cf0dd6666487723c8fa
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-07-05 11:14:46 -0300

    test: Adiciona testes para o use case de gerar receita

---
+++ tests/CommomTestUtilities/Dtos/GenerateRecipeDtoBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using LivroDeReceitas.Domain.Dtos;
+using LivroDeReceitas.Domain.Enums;
+using LivroDeReceitas.Domain.ValueObjects;
+
+namespace CommomTestUtilities.Dtos
+{
+    public class GenerateRecipeDtoBuilder
+    {
+        public static GeneratedRecipeDto Build()
+        {
+            return new Faker<GeneratedRecipeDto>()
+                 .RuleFor(recipe => recipe.Title, faker => faker.Lorem.Word())
+                 .RuleFor(recipe => recipe.Ingredients, faker => faker.Make(LivroDeReceitasRuleConstants.MAXIMUM_INGREDIENTS_GENERATE_RECIPE, () => faker.Commerce.ProductName()))
+                 .RuleFor(recipe => recipe.CookingTime, faker => faker.PickRandom<CookingTime>())
+                 .RuleFor(recipe => recipe.Difficulty, faker => Difficulty.Low)
+                 .RuleFor(recipe => recipe.Instructions, faker => faker.Make(1, () => new GeneratedInstructionDto
+                 {
+                     Step = 1,
+                     Text = faker.Lorem.Paragraph()
+                 }));
+        }
+    }
+}
---
+++ tests/UseCases.Test/Recipe/Generate/GenerateRecipeUseCaseTest.cs
@@ -0,0 +1,54 @@
+﻿using CommomTestUtilities.Dtos;
+using CommomTestUtilities.OpenAI;
+using CommomTestUtilities.Requests;
+using LivroDeReceitas.Application.UseCases.Recipe.Generate;
+using LivroDeReceitas.Domain.Dtos;
+using LivroDeReceitas.Domain.ValueObjects;
+using LivroDeReceitas.Exceptions;
+using LivroDeReceitas.Exceptions.ExceptionsBase;
+using Shouldly;
+
+namespace UseCases.Test.Recipe.Generate
+{
+    public class GenerateRecipeUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var dto = GenerateRecipeDtoBuilder.Build();
+
+            var request = RequestGenerateRecipeJsonBuilder.Build();
+
+            var useCase = CreateUseCase(dto);
+
+            var result = await useCase.Execute(request);
+
+            result.ShouldNotBeNull();
+            result.Title.ShouldBe(dto.Title);
+            result.CookingTime.ShouldBe((LivroDeReceitas.Comunication.Enums.CookingTime)dto.CookingTime);
+            result.Difficulty.ShouldBe((LivroDeReceitas.Comunication.Enums.Difficulty)dto.Difficulty);
+        }
+
+        [Fact]
+        public async Task Error_DuplicatedIngredients()
+        {
+            var dto = GenerateRecipeDtoBuilder.Build();
+
+            var request = RequestGenerateRecipeJsonBuilder.Build(LivroDeReceitasRuleConstants.MAXIMUM_INGREDIENTS_GENERATE_RECIPE - 1);
+            request.Ingredients.Add(request.Ingredients.First());
+
+            var useCase = CreateUseCase(dto);
+
+            var exception = await Should.ThrowAsync<ErrorOnValidationException>(async () => await useCase.Execute(request));
+            exception.GetErrorMessages().Count.ShouldBe(1);
+            exception.GetErrorMessages().ShouldContain(ResourceMessagesException.DUPLICATED_INGREDIENTS_IN_LIST);
+        }
+
+        private static GenerateRecipeUseCase CreateUseCase(GeneratedRecipeDto dto)
+        {
+            var generator = GenerateRecipeAIBuilder.Build(dto);
+
+            return new GenerateRecipeUseCase(generator);
+        }
+    }
+}
